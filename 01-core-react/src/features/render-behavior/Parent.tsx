6b22e3b1cf055c916f858bfc26cf43012bb6300f
sidram403 <siddharthpatil9108@gmail.com>
2026-02-16 22:58:24 +0530

    feat: add theme state to demostrate unnecessary child re-renders

--- 01-core-react/src/features/render-behavior/Parent.tsx
+++ 01-core-react/src/features/render-behavior/Parent.tsx
@@ -6,14 +6,29 @@ import Child from "./Child";
  *
  * Holds state and triggers re-renders
  * Used to visualize React's render propagation behavior
+ *
+ * Now includes:
+ * - count state
+ * - theme state  (unrelated to child)
+ *
+ * Purpose :
+ * Demonstrate how even unrelated state changes
+ * cause child re-renders
  */
 function Parent(): React.ReactElement {
   const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
   console.log("Parent Rendered");
 
   return (
-    <section style={{ padding: "2rem" }}>
+    <section
+      style={{
+        padding: "2rem",
+        backgroundColor: theme === "light" ? "#fff" : "#222",
+        color: theme === "light" ? "#000" : "#fff",
+      }}
+    >
       <h2>Render Behavior Demo</h2>
 
       <p>
@@ -23,8 +38,18 @@ function Parent(): React.ReactElement {
       {/*
               Functional update prevents stale state issues.
               Always preferred in production for state derived from previous state.  
-            */}
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      */}
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Increment Count
+      </button>
+
+      <button
+        onClick={() =>
+          setTheme((prev) => (prev === "light" ? "dark" : "light"))
+        }
+      >
+        Toggel Theme
+      </button>
       <Child />
     </section>
   );
